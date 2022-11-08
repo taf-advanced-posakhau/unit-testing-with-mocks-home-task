3d8fd4210b3640758fa63f0fb9ed7e2a0a1b7c7e
Aliaksandr Posakhau <Aliaksandr_Posakhau@epam.com>
2022-11-08 13:49:19 +0300

    addition and update of unit tests for hometask 5

--- src/data_handlers/unit_tests.test.js
+++ src/data_handlers/unit_tests.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable quotes */
 /* eslint-disable semi */
 /* eslint-disable arrow-spacing */
 /* eslint-disable eol-last */
@@ -12,8 +13,11 @@
 /* eslint-disable camelcase */
 import { expect } from 'chai'
 import UserDataHandler from './user_data_handler'
-import users from '../../data/users.json'
+import users from '../../data/users.json' 
 const user_data_handler = new UserDataHandler()
+const assert = require('assert');
+const { AssertionError } = require('assert');
+
 
 describe('Verification of loadUsers() method', () => {
   it('verification full list of users which loadUsers() method updating', async () => {
@@ -22,7 +26,44 @@ describe('Verification of loadUsers() method', () => {
   })
 })
 
-describe('Verification of getUserEmailsList() method', () => {
+describe('Verification of getNumberOfUsers() method', () => {
+  it('verification of number of all users', async () => {
+    await user_data_handler.loadUsers() 
+    expect(user_data_handler.getNumberOfUsers()).to.deep.equal(users.length)
+  })
+})
+
+describe('Verification of isMatchingAllSearchParams() method - positive case', () => {
+it('verification isMatchingAllSearchParams() method', async () => { 
+  let searchParameter = {
+    name: 'Leanne Graham',
+    username: 'Bret'
+  }       
+  await user_data_handler.loadUsers()
+  expect(user_data_handler.isMatchingAllSearchParams(user_data_handler.users[0], searchParameter), user_data_handler.isMatchingAllSearchParams(users[0], searchParameter))    
+})
+})
+
+describe('Verification of isMatchingAllSearchParams() method - negative case', () => {
+it('verification isMatchingAllSearchParams() method', async () => { 
+  let searchParameter = {
+    name: 'William Forrester',
+    username: 'Lucky'
+  }       
+  await user_data_handler.loadUsers()
+  expect(user_data_handler.isMatchingAllSearchParams(user_data_handler.users[0], searchParameter), user_data_handler.isMatchingAllSearchParams(users[0], searchParameter))    
+})
+})
+
+describe('Verification of findUsers() method', () => {
+it('verification findUsers() method', async () => {      
+  await user_data_handler.loadUsers() 
+  user_data_handler.findUsers({ email: users[0].email })
+  expect(user_data_handler.findUsers({ email: users[0].email })).to.deep.equal(user_data_handler.findUsers({ email: user_data_handler.users[0].email }))     
+})
+})
+ 
+describe('Verification of getUserEmailsList() method - positive case', () => {
     it('verification of list of all users emails', async () => {
 
         const formActualEmailsList = async () => {
@@ -40,27 +81,18 @@ describe('Verification of getUserEmailsList() method', () => {
     })
 })
 
-describe('Verification of getNumberOfUsers() method', () => {
-    it('verification of number of all users', async () => {      
-      expect(user_data_handler.getNumberOfUsers()).to.deep.equal(users.length)
-    })
-})
-
-describe('Verification of isMatchingAllSearchParams() method', () => {
-  it('verification isMatchingAllSearchParams() method', async () => { 
-    let searchParameter = {
-      name: 'Leanne Graham',
-      username: 'Bret'
-    }       
-    await user_data_handler.loadUsers()
-    expect(user_data_handler.isMatchingAllSearchParams(user_data_handler.users[0], searchParameter), user_data_handler.isMatchingAllSearchParams(users[0], searchParameter))    
+describe('Verification of getUserEmailsList() method - negative case', () => {
+  it('verification of list of all users emails', async () => {
+      user_data_handler.users = {};
+      try {
+        user_data_handler.getUserEmailsList();  
+        assert.fail('expected exception not thrown');  
+      } catch (e) { 
+        if (e instanceof AssertionError) {          
+          throw e;
+        }
+        assert.equal(e.message, 'this.users.map is not a function');
+      }     
   })
 })
-
-describe('Verification of findUsers() method', () => {
-  it('verification findUsers() method', async () => {      
-    await user_data_handler.loadUsers() 
-    user_data_handler.findUsers({ email: users[0].email })
-    expect(user_data_handler.findUsers({ email: users[0].email })).to.deep.equal(user_data_handler.findUsers({ email: user_data_handler.users[0].email }))     
-  })
-})
+ 
